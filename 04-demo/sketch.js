49e60dfa992c5f1b3f3fc80f71276f97133e3e57
sauderayrton-maker <sauder.ayrton@gmail.com>
2026-03-04 11:03:10 -0600

    Update sketch.js

--- 04-demo/sketch.js
+++ 04-demo/sketch.js
@@ -8,29 +8,48 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background(ball.r, ball.g, ball.b);
   for (let ball of ballArray) {
     //move
     ball.x += ball.dx;
     ball.y += ball.dy;
 
     //display
+    fill(ball.r, ball.g, ball.b);
     circle(ball.x, ball.y, ball.radius *2);
+
+    //teleport
+    if (ball.x - ball.radius > width) {
+      ball.x -= width - ball.radius;
+    }
+    if (ball.x + ball.radius < 0){
+      ball.x += width + ball.radius;
+    }
+    if (ball.y - ball.radius > height)  {
+      ball.y -= height - ball.radius;
+    }
+    if (ball.y + ball.radius < 0) {
+      ball.y += height + ball.radius;
+    }
+
   }
 }
 
 function mousePressed() {
-  spawnBall();
+  spawnBall(mouseX, mouseY);
 }
 
 
-function spawnBall() {
+function spawnBall(_x, _y) {
   let someBall = {
-    x: random(width),
-    y: random(height),
+    x: _x,
+    y: _y,
     dx: random(-5, 5),
     dy: random(-5, 5),
-    radius: random(10, 30)
+    radius: random(10, 30),
+    r: random(0, 255),
+    g: random(0, 255),
+    b: random(0, 255),
   };
-  ballArray.push(someBall)
+  ballArray.push(someBall);
 }
