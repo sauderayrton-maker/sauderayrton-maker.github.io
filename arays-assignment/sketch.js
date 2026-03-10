be02832a8e37bce996c9deb8967af9441debd71c
sauderayrton-maker <sauder.ayrton@gmail.com>
2026-03-10 11:01:35 -0600

    tried adding a car added, controls and left lane logic

--- arays-assignment/sketch.js
+++ arays-assignment/sketch.js
@@ -8,8 +8,15 @@
 const ROADWIDTH = 100;
 const LINESPEED = 50; 
 
+let fast = false;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  let cX = width;
+  let cY = height;
+  let cW = 45; 
+  let cH = 75;
+
 }
 
 function draw() {
@@ -17,9 +24,35 @@ function draw() {
   fill(0);
   rect(width/2 - ROADWIDTH, 0, ROADWIDTH * 2, height);
   roadLines();
+  control();
+  goFast();
 }
 
-function roadLines(y){
+function roadLines(){
   fill(255, 255, 0);
-  rect(width / LINEWIDTH, height- 60, 20, 60);
+  rect(width/2, height/2, 20, height);
+}
+
+function goFast(){
+  if (fast){
+    mergeRight();
+  }
+}
+
+function control(){
+  if (keyIsDown(32)) {
+    fast = true;
+  }
+  else{
+    fast = false;
+  }
+}
+
+function mergeRight(){
+  console.log('vroom');
+}
+
+function car(){
+  fill(255, 0, 0);
+  rect(cX, cY, cW, cH);
 }
