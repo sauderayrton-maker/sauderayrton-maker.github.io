44e58cc8cf7df2fb0a420a3a6d6ee6410171f11a
sauderayrton-maker <sauder.ayrton@gmail.com>
2026-03-16 11:02:16 -0600

    updated hud and colour scheme

--- arays-assignment/sketch.js
+++ arays-assignment/sketch.js
@@ -47,7 +47,7 @@ function draw() {
     start();
   }
   if (gameState === 'play'){
-    background(10, 25, 15);
+    background(160, 75, 45);
     drawRoad();
     roadLines();
     car();
@@ -66,15 +66,15 @@ function keyPressed() {
 
 //----- MAKES THE ROAD -----//
 function drawRoad(){
-  fill(215, 215, 200);
+  fill(210, 180, 140);
   rect(width/2 - ROADWIDTH - ROADWIDTH / 10, 0, ROADWIDTH * 2 + 20, height);
-  fill(0);
+  fill(45, 45, 50);
   rect(width/2 - ROADWIDTH, 0, ROADWIDTH * 2, height);
 }
 
 //----- MAKES THE ROAD LINES -----//
 function roadLines(){
-  fill(255, 210, 100); //yellow
+  fill(255, 230, 150);
   for (let i = -100; i < height + 100; i += 100){
     rect(width / 2 - 5, i + lineLoop, 10, 50);
   }
@@ -134,16 +134,16 @@ function mergeRight(){
 //----- DRAWS THE PLAYER CAR -----//
 function car(){
   noStroke();
-  fill(200, 0, 0);
+  fill(200, 177, 95);
   rect(cX - cW/2, cY - cH/2, cW, cH, 8);
 
-  fill(255, 0, 0);
+  fill(255, 230, 150);
   rect(cX - cW/2 + 5, cY - cH/2 + 15, cW - 10, cH - 30, 5);
 
-  fill(50, 50, 80);
+  fill(25, 35, 45);
   rect(cX - cW/2 + 7, cY - cH/2 + 20, cW - 14, 15, 2);
 
-  fill(50, 50, 80);
+  fill(25, 35, 45);
   rect(cX - cW/2 + 7, cY - cH/2 + 50, cW - 14, 10, 2);
 }
 
@@ -152,19 +152,19 @@ function start(){
   background(15, 15, 15);
   strokeWeight(2);
   stroke(60, 60, 80);
-  fill(40, 40, 50, 200);
+  fill(25, 20, 20, 200);
   rect(width / 2 - 250, height / 2 - 150, 500, 300, 20);
   noStroke();
-  fill(0, 200, 255);
+  fill(255, 160, 60);
   textStyle(BOLD);
   textSize(45);
   textAlign(CENTER);
   text("CAR GAME", width / 2, height / 2 - 80);
   textStyle(normal);
-  fill(220);
+  fill(255, 160, 60);
   textSize(22);
   text("Press Enter to Start", width / 2, height / 2 + 10);
-  fill(150);
+  fill(255, 160, 60);
   textSize(18);
   text("Press Space to Pass", width / 2, height / 2 + 50);
   
@@ -182,15 +182,20 @@ function end(){
 //----- HUD -----//
 function hud(){
 //----- BACKING -----//
-  strokeWeight(2);
-  stroke(60, 60, 80);
-  fill(40, 40, 50, 200);
-  rect(hudX, hudY, hudW, hudH, 20);
+  fill(200, 205, 215);
+  strokeWeight(3);
+  stroke(130, 135, 145);
+  circle(hudX + hudW / 2 - hudW / 4, hudY + hudH / 2 - hudH / 11 , hudW / 2 - hudH / 20);
+  circle(hudX + hudW / 2 + hudW / 4, hudY + hudH / 2 - hudH / 11 , hudW / 2 - hudH / 20);
+  stroke(245, 250, 255);
+  fill(15, 15, 18, 230);
+  circle(hudX + hudW / 2 - hudW / 4, hudY + hudH / 2 - hudH / 11 , hudW / 2 - hudH / 8);
+  circle(hudX + hudW / 2 + hudW / 4, hudY + hudH / 2 - hudH / 11 , hudW / 2 - hudH / 8);
   noStroke();
  
   //----- SPEEDLABLE -----//
   textAlign(CENTER);
-  fill(100, 110, 140);
+  fill(180, 180, 190);
   textSize(11);
   textStyle(BOLD);
   text("SPEED", hudX + hudW / 4, hudY + 45);
@@ -198,13 +203,13 @@ function hud(){
  
   //----- COLOURCHANGING -----//
   if (currentSpeed >= 150) {
-    fill(255, 60, 60);
+    fill(255, 80, 80);
   } 
   else if (currentSpeed >= 100) {
-    fill(255, 220, 60);
+    fill(255, 200, 80);
   } 
   else {
-    fill(200, 210, 255);
+    fill(120, 220, 255);
   }
 
   textSize(52);
@@ -221,17 +226,17 @@ function hud(){
   // line(hudX + 25, hudY + 160, hudX + hudW - 25, hudY + 160);
   // noStroke();
  
-  fill(100, 110, 140);
+  fill(180, 180, 190);
   textSize(11);
   textStyle(BOLD);
   text("DISTANCE", hudX + hudW - hudW / 4, hudY + 45);
   textStyle(NORMAL);
  
   if (fast) {
-    fill(0, 200, 255);
+    fill(255, 80, 80);
   } 
   else {
-    fill(200, 210, 255);
+    fill(120, 220, 255);
   }
   textSize(36);
   textStyle(BOLD);
