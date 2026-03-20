b452f0f687c7628c3cb1af8a434dc47fe61b060c
Ayrton Sauder <>
2026-03-19 22:16:17 -0600

    started work on assignment

--- 2d-arrays/sketch.js
+++ 2d-arrays/sketch.js
@@ -1,9 +1,22 @@
 // 2d arrays assignment
 
+let grid;
+const GRIDBOX = 10;
+let gridSize;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  checkTheSize();
 }
 
 function draw() {
   background(220);
+}
+
+function checkTheSize() {
+  if (width > height) {
+    gridSize = width / GRIDBOX;
+  } else {
+    gridSize = height / GRIDBOX;
+  }
 }
