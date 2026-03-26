f18b59950e617112de20f4f75bbb3d1e7a69a9e8
Ayrton-Sauder <sauder.ayrton@gmail.com>
2026-03-26 09:32:19 -0600

    Remove redundant newGrid initialization
    Removed unnecessary line that initializes a new grid in the insideWall function.

--- 2d-arrays/sketch.js
+++ 2d-arrays/sketch.js
@@ -49,7 +49,6 @@ function outsideWall(cols, rows) {
 
 function insideWall(cols, rows) {
     for (let y = 1; y < rows - 1; y++) {
-      newGrid.push([]);
       for (let x = 1; x < cols - 1; x++) {
         if (random(100) > 50) {
           grid[y][x] = 1;
