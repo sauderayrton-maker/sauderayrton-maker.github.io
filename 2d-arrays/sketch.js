b7b1932d5136d05e53ee7a2b20e92885b1618fed
Ayrton-Sauder <sauder.ayrton@gmail.com>
2026-03-26 09:11:29 -0600

    Refactor insideWall function to initialize grid correctly

--- 2d-arrays/sketch.js
+++ 2d-arrays/sketch.js
@@ -49,13 +49,15 @@ function outsideWall(cols, rows) {
 
 function insideWall(cols, rows) {
   let newGrid = [];
-  for (let y = 1; y < rows - 1; y++) {
-    newGrid.push([]);
-    for (let x = 1; x < cols - 1; x++) {
-      if (random(100) > 50) {
-        newGrid[y][x].push(1);
-      } else {
-        newGrid[y][x].push(0);
+  for (let wallNum = 0; wallNum <= inside; wallNum++){
+    for (let y = 1; y < rows - 1; y++) {
+      newGrid.push([]);
+      for (let x = 1; x < cols - 1; x++) {
+        if (random(100) > 50) {
+          newGrid[y][x] = 1;
+        } else {
+          newGrid[y][x] = 0;
+        }
       }
     }
   }
