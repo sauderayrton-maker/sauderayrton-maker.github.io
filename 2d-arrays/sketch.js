8dbe31efc3139fee0665d981a71d099e2922a789
sauderayrton-maker <sauder.ayrton@gmail.com>
2026-03-26 10:39:34 -0600

    added preload and made it work

--- 2d-arrays/sketch.js
+++ 2d-arrays/sketch.js
@@ -1,11 +1,31 @@
-// rect grid
+// Battle Tanks
+// Ayrton Sauder
+// march 26 2026
+//
+// Extra for Experts:
+// - the gratest animations of all time mixed with a soundtrack that would make a grown kid cry all preloaded ofc textures never seen before to such quality
+// sound effects making realism a understatement amd to top it all off the best online support simce sliced bread
 
 const cellSize = 50;
 let inside;
 let grid;
 let rows, cols;
+let player = {
+  x: 0,
+  y: 0,
+};
+let wall;
+let ground;
+let boom;
+
+function preload() {
+  wall = loadImage("brick.png");
+  ground = loadImage("grass.png");
+  boom = loadImage("fireball.jpg");
+}
 
 function setup() {
+  noStroke();
   createCanvas(windowWidth, windowHeight);
   rows = floor(height / cellSize);
   cols = floor(width / cellSize);
@@ -23,11 +43,11 @@ function displayGrid() {
   for (let y = 0; y < rows; y++) {
     for (let x = 0; x < cols; x++) {
       if (grid[y][x] === 0) {
-        fill("white");
-      } else {
-        fill("black");
+        image(ground, x * cellSize, y * cellSize, cellSize);
       }
-      square(x * cellSize, y * cellSize, cellSize);
+      if (grid[y][x] === 1) {
+        image(wall, x * cellSize, y * cellSize, cellSize);
+      }
     }
   }
 }
@@ -44,17 +64,17 @@ function outsideWall(cols, rows) {
       }
     }
   }
+  return newGrid;
 }
 
 function insideWall(cols, rows) {
-    for (let y = 1; y < rows - 1; y++) {
-      for (let x = 1; x < cols - 1; x++) {
-        if (random(100) > 50) {
-          grid[y][x] = 1;
-        } else {
-          grid[y][x] = 0;
-        }
+  for (let y = 1; y < rows - 1; y++) {
+    for (let x = 1; x < cols - 1; x++) {
+      if (random(100) > 50) {
+        grid[y][x] = 1;
+      } else {
+        grid[y][x] = 0;
       }
+    }
   }
-  return newGrid;
 }
