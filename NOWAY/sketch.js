3c58f72551d40c554c559a3f9d32eeb10cefac02
Ayrton-Sauder <sauder.ayrton@gmail.com>
2026-04-14 10:28:30 -0600

    locked in

--- NOWAY/sketch.js
+++ NOWAY/sketch.js
@@ -1,15 +1,47 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// walker oop demo
 
+class Walker {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.color = "red";
+    this.speed = 5;
+    this.diameter = 2;
+  }
+
+  display() {
+    stroke(this.color);
+    fill(this.color);
+    circle(this.x, this.y, this.diameter);
+  }
+
+  move() {
+    let choice = random(100);
+    if (choice < 25) {
+      this.x += this.speed;
+    } else if (choice < 50) {
+      this.x -= this.speed;
+    } else if (choice < 75) {
+      this.y += this.speed;
+    } else if (choice < 100) {
+      this.y -= this.speed;
+    }
+  }
+}
+
+let tyler;
+let audrey;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  tyler = new Walker(width / 2, height / 2);
+  audrey = new Walker(300, 500);
 }
 
 function draw() {
-  background(220);
+  tyler.move();
+  tyler.display();
+  audrey.color = "blue";
+  audrey.move();
+  audrey.display();
 }
